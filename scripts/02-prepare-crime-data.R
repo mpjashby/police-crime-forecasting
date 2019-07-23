0831bb47aee118502d710b5ef2153b8880e22ecd
Matt Ashby <matthew.ashby@ntu.ac.uk>
2019-07-23 11:35:13 +0100

    Add H3 models

--- scripts/02-prepare-crime-data.R
+++ scripts/02-prepare-crime-data.R
@@ -1,16 +1,16 @@
 # This file downloads data from the Crime Open Database for cities for which
 # data are available for at least 10 years and stores them in a single table,
 # adding a variable corresponding to the district (precinct etc.) in which the
-# crime occurred.
+# crime occurred. Fort Worth is excluded because the data only have dates, not
+# times.
 
 
 
 # DOWNLOAD CRIME DATA
 crimes <- get_crime_data(
 	years = 2010:2018, # 5 years training + 1 year forecast horizon + 3 years test
-	cities = c("Austin", "Chicago", "Detroit", "Fort Worth", "Kansas City", 
-						 "Los Angeles", "Louisville", "New York", "San Francisco", 
-						 "Tucson"),
+	cities = c("Austin", "Chicago", "Detroit", "Kansas City", "Los Angeles", 
+						 "Louisville", "New York", "San Francisco", "Tucson"),
 	type = "core",
 	output = "sf"
 ) %>% 
